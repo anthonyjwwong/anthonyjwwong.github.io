00e2a8d0ba20d34a8302231f62968e7ce47c44e9
Anthony <anthonywongjw@gmail.com>
2018-07-14 19:41:49 -0700

    Changed

--- js/script.js
+++ js/script.js
@@ -1,41 +1,10 @@
-let modal = document.getElementById('myModal');
-let btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+const button = document.querySelector('button');
+const span = document.querySelector('span');
+let count=0;
 
 
-//Speech
-let intro = document.querySelector('.intro');
-let hello = document.querySelector('.hello');
-let dev = document.querySelector('.webDev');
-let thanks = document.querySelector('.thanks');
-let employ = document.querySelector('.employ');
+button.addEventListener('click', function() {
 
-function helloIntro() {
-    setTimeout(function(){
-      hello.classList.toggle("displayNone");
-      employ.classList.toggle("displayNone");
-    },2500);
-
-    setTimeout(function(){
-        dev.classList.remove("displayNone")
-        thanks.classList.toggle("displayNone");
-    }, 2500);
-
-
-}
-
-helloIntro();
+    count++;
+    span.innerHTML = count;
+})
